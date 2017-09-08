37feace0e859c2d1c22aa602849a1b8f919443ae
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2017-09-08 19:46:15 +0530

    Create kalai.c

---
+++ kalai.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d",even number);
+}
+else
+{
+printf("%d",odd number);
+}
+}

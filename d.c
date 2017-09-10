23a8435230c16309c59324104698c057aef17ac2
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2017-09-10 18:59:47 +0530

    Create d.c

---
+++ d.c
@@ -0,0 +1,15 @@
+#inculde<stdio.h>
+#include<conoi.h>
+void main()
+{
+int n count=0;
+printf("enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+//n=n/10;
+n/=10;
+++count;
+}
+printf("%d",count);
+}

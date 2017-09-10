0d7f6d92462c6f20b4471983726e62d6d6007ac4
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2017-09-10 16:29:52 +0530

    Create V.C

---
+++ V.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ch;
+printf("enter the character");
+scanf("%d",&ch);
+if(ch='a'!!ch='e'!!ch='i'!!ch='o'!!ch='u'!!ch='A'!!ch='E'!!ch='I'!!ch='O'!!ch='U')
+{
+printf(" character is vowel letter");
+}
+else
+{
+printf("character is cosonant letter");
+}
+}

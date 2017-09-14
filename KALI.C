3c86a265ceb22eb0e485b9292f5b58f4ae70bd72
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2017-09-14 19:54:25 +0530

    Create KALI.C

---
+++ KALI.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conoi.h>
+int main()
+{
+int base,exponent;
+long long result=1;
+printf("enter the base number");
+scanf("%d",&base);
+printf("enter an exponent");
+scanf("%d",&exponenet);
+while(exponent!=0)
+{
+result*=base;
+__exponent;
+}
+printf("answer=11d",result);
+return 0;
+}

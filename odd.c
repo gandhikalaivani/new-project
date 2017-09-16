cc11cfd7474cbf3b58584d310b8fdc6cd6d4f193
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2017-09-16 19:34:57 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int f,l,i;
+printf("enter the first interval");
+scanf("%d",&f);
+printf("enter the second interval");
+scanf("%d",&l);
+for(i=f;i<=l;i++);
+{
+if(i==1)
+{
+printf("%d odd number",i)
+}
+}
+}

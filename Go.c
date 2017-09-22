a86ba7066b4e44795353c63d76e038b0bb8c7a02
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2017-09-22 19:21:15 +0530

    Create Go.c

---
+++ Go.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n1,n2;
+printf("enter the two integer");
+scanf("%d%d",&n1,&n2);
+for(i=1;i<=n1&&i<=n2;i++)
+{
+if(n1/i==0&&n2/i==0)
+gcd=i
+}
+printf("%d and %d is,n,n2,gcd");
+getch();
+}

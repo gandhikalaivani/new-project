4cc9586b2a2dc4cc43984751730d269dd4819d77
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2018-01-19 15:57:52 +0530

    Create gh1.c

---
+++ gh1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,num,smallest;
+printf("enter the element");
+scanf("%d",&num);
+for(i=0;i<num;i++);
+scanf("%d",&num);
+printf("%d",smallest num);
+return 0;
+}

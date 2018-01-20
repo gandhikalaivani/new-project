2c2a7c4f86c00cc5fada36a6c16dd430ceb99ecf
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2018-01-20 19:28:32 +0530

    Create hours.c

---
+++ hours.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int h1,h2,m,m2,er,re;
+printf("enter the number");
+scanf("%d\t%d",&h1,&m1);
+printf("enter the number");
+scanf("%d\t%d",&h2,&m2);
+er=h1-h2;
+re=m1-m2;
+printf("%d\t%d",er,re);
+return 0;
+}

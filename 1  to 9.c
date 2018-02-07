b52ca6963f0bb72d11a37d76f84e20447bc7a9cc
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2018-02-07 15:34:10 +0530

    Create 1  to 9.c

---
+++ 1  to 9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a[10],n,i;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("enter the number");
+	for(i-0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	if(n=>0&&n<=9)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}

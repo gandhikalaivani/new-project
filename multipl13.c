52e6315bb1c4cc2fe8f353250d13c4558ac4e557
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2018-02-28 16:40:55 +0530

    Create multipl13.c

---
+++ multipl13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int s;
+	scanf("%d",&s);
+	    if(s%13==0)
+	    {
+	        printf("yes");
+	    }
+	    else
+	    {
+	        printf("no");
+	    }
+	return 0;
+}

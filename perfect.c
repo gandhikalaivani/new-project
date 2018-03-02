de31ec99161627a5d8e5f213ebbdc666b48e39fb
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2018-03-02 19:20:21 +0530

    Create perfect.c

---
+++ perfect.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  scanf("%d%d",&a,&b);
+  if(a==b)
+  {
+      printf("perfect squre");
+  }
+  else
+  {
+      printf(" not perfect squre");
+  }
+
+    return 0;
+}
+
+
+

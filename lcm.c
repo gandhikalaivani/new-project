890578c3770713f1070fe709d111bcb71ee02167
gandhikalaivani <31769250+gandhikalaivani@users.noreply.github.com>
2018-03-12 16:45:14 +0530

    Create lcm.c

---
+++ lcm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,gcd,i,lcm;
+	scanf("%d%d",&a,&b);
+	for(i=1;i<a && i<b;i++)
+	{
+		if(a%i==0 && b%i==0)
+		gcd=i;
+	}
+	gcd=i;
+	lcm=(a*b)/gcd;
+	printf("lcm is:%d",lcm);
+	return 0;
+}
